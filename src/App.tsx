529a0a39b7c0848a8ca38931823181c08fd09e44
essfadi <oussamaessfadi@gmail.com>
2022-03-26 13:41:43 +0100

    wa pushi

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppAppBar from './modules/views/AppAppBar'
+
+function App() {
+  return (
+    <div className="App">
+      <AppAppBar />
+    </div>
+  );
+}
+
+export default App;
