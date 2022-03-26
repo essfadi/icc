da185087a44e7749105ddd1ba1262cfe1a33a7b9
002FOX <s.elarif@aui.ma>
2022-03-26 22:59:47 +0100

    yes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import './App.css';
-import {BrowserRouter as Router, Switch, Route} from  'react-router-dom'
-import Home from './pages';
-import SigninPage from './pages/signin';
+import React from 'react';
+import {Form} from './components/Form';
+
 
 function App() {
+  
   return (
-    <Router>
-      <Switch>
-        <Route path='/' component={Home} exact />
-        <Route path='/signin' component={SigninPage} exact />
-      </Switch>
-    </Router>
+    <div className="App">
+     <Form />
+    </div>
   );
 }
 
